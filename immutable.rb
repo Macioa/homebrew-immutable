242cc390a7a8c890a3b10e031314f3eb6c8dc1a1
Ryan Montgomery <ryan.montgomery@frayt.com>
2024-08-30 17:32:02 -0500

    add tool versions to init

--- immutable.rb
+++ immutable.rb
@@ -1,8 +1,8 @@
 class Immutable < Formula
     desc "Setup development environment with Homebrew, asdf, Erlang, Elixir, Node.js, and more"
     homepage "https://github.com/macioa/immutablestack"
-    url "file://#{File.expand_path("ImmutableStack-v0.0.0.tar.gz", __dir__)}"
-    sha256 "9b55b3810507268b596e65f6202b76b04385ab82954b35d867d73f9ea0cd6889"
+    url "file://#{File.expand_path("ImmutableStack-v0.0.1.tar.gz", __dir__)}"
+    sha256 "a014034208b1b098bd203dce408e2820a829d1763494c01f02512ed7f5641e9e"
     license "Apache-2.0"
     version "0.0.0"
   
@@ -14,6 +14,7 @@ class Immutable < Formula
       bin.install "dev_kit_macos.sh"
       bin.install "init_proj.sh"
       bin.install "versions.sh"
+      bin.install "gen_toolkit.sh"
       (bin/"immutable").write <<~EOS
         #!/bin/bash
         if [ "$1" == "-install" ]; then
@@ -22,6 +23,7 @@ class Immutable < Formula
           "$(brew --prefix)/bin/dev_kit_macos.sh"
         elif [ "$1" == "-init" ]; then
           "$(brew --prefix)/bin/init_proj.sh" "$2"
+          "$(brew --prefix)/bin/gen_toolkit.sh" "$2_umbrella"
         else
           echo "Usage: immutable -install"
         fi
