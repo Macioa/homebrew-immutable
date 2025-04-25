0e6783681ab7c61ef8deb0236ac19cf506f75e3e
Ryan Montgomery <ryan.montgomery@frayt.com>
2025-04-24 22:07:37 -0500

    0.3.21

--- immutable.rb
+++ immutable.rb
@@ -1,8 +1,8 @@
 class Immutable < Formula
     desc "Setup development environment with Homebrew, asdf, Erlang, Elixir, Node.js, and more"
     homepage "https://github.com/macioa/immutablestack"
-    url "file://#{File.expand_path("ImmutableStack-v0.3.201.tar.gz", __dir__)}"
-    sha256 "8ba1fdd59e2e80d9d76724c0e74a8c4abd3fb39867870b7dc1012b761ca1e8b6"
+    url "file://#{File.expand_path("ImmutableStack-v0.3.210.tar.gz", __dir__)}"
+    sha256 "af906426425135ab3313163d1457dbb1b295b904821d6577cd0e1a88ebb33fa5"
     license "Apache-2.0"
     version "0.0.0"
   
